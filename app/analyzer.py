d27c07ffdd4863718fd389fc4b1bba39482128d1
Anmol Rathore <82675610+Anmolrathore2002@users.noreply.github.com>
2026-02-28 19:24:08 +0530

    Update analyzer.py

--- app/analyzer.py
+++ app/analyzer.py
@@ -4,11 +4,27 @@ import json
 with open("data/normal_ranges.json") as f:
     normal_ranges = json.load(f)
 
+def extract_patient_info(text):
+    name = age = gender = None
+
+    name_match = re.search(r"(Name|Patient Name)\s*[:\-]?\s*([A-Za-z ]+)", text, re.I)
+    age_match = re.search(r"(Age)\s*[:\-]?\s*(\d+)", text, re.I)
+    gender_match = re.search(r"(Gender|Sex)\s*[:\-]?\s*(Male|Female)", text, re.I)
+
+    if name_match:
+        name = name_match.group(2).strip()
+    if age_match:
+        age = age_match.group(2).strip()
+    if gender_match:
+        gender = gender_match.group(2).strip()
+
+    return name, age, gender
+
 def extract_values(text):
     extracted = {}
     for test in normal_ranges.keys():
         pattern = rf"{test}\s*[:\-]?\s*([\d\.]+)"
-        match = re.search(pattern, text, re.IGNORECASE)
+        match = re.search(pattern, text, re.I)
         if match:
             extracted[test] = float(match.group(1))
     return extracted
@@ -18,23 +34,9 @@ def classify_severity(test, value):
     max_val = normal_ranges[test]["max"]
 
     if value < min_val:
-        diff = min_val - value
-        if diff > 3:
-            return "Severe Low"
-        elif diff > 1:
-            return "Moderate Low"
-        else:
-            return "Mild Low"
-
+        return "Mild Low"
     elif value > max_val:
-        diff = value - max_val
-        if diff > 50:
-            return "Severe High"
-        elif diff > 20:
-            return "Moderate High"
-        else:
-            return "Mild High"
-
+        return "Mild High"
     return "Normal"
 
 def calculate_risk_score(analysis):
